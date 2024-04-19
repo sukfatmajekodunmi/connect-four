f3788b999c00cd4990cb76df35077dc6804e3ece
Selim Majekodunmi <165806675+sukfatmajekodunmi@users.noreply.github.com>
2024-04-19 20:21:54 +0100

    Update play.py

--- play.py
+++ play.py
@@ -1,52 +1,59 @@
-from connect4 import *
+from connect_four import Game
 
-def main():
-    """ Play a game!
-    """
-    
-    g = Game()
-    g.printState()
-    player1 = g.players[0]
-    player2 = g.players[1]
-    
-    win_counts = [0, 0, 0] # [p1 wins, p2 wins, ties]
-    
-    exit = False
-    while not exit:
-        while not g.finished:
-            g.nextMove()
-        
-        g.findFours()
-        g.printState()
-        
-        if g.winner == None:
-            win_counts[2] += 1
-        
-        elif g.winner == player1:
-            win_counts[0] += 1
-            
-        elif g.winner == player2:
-            win_counts[1] += 1
-        
-        printStats(player1, player2, win_counts)
-        
-        while True:
-            play_again = str(input("Would you like to play again? "))
-            
-            if play_again.lower() == 'y' or play_again.lower() == 'yes': 
-                g.newGame()
-                g.printState()
-                break
-            elif play_again.lower() == 'n' or play_again.lower() == 'no':
-                print("Thanks for playing!")
-                exit = True
-                break
-            else:
-                print("I don't understand... "),
-        
-def printStats(player1, player2, win_counts):
-    print("{0}: {1} wins, {2}: {3} wins, {4} ties".format(player1.name,
-        win_counts[0], player2.name, win_counts[1], win_counts[2]))
-        
-if __name__ == "__main__": # Default "main method" idiom.
-    main()
+
+def play_game():
+    """Start and play a game of Connect4."""
+    game_instance = Game()
+    game_instance.print_state()
+
+    player1, player2 = game_instance.players[:2]
+    score_board = [0, 0, 0]  # [player1 wins, player2 wins, ties]
+
+    game_active = True
+    while game_active:
+        while not game_instance.finished:
+            game_instance.next_move()
+
+        game_instance.find_fours()
+        game_instance.print_state()
+
+        update_scoreboard(game_instance, player1, player2, score_board)
+        display_stats(player1, player2, score_board)
+
+        game_active = ask_for_rematch(game_instance)
+
+
+def update_scoreboard(game_instance, player1, player2, score_board):
+    """Update the scoreboard based on the game outcome."""
+    if game_instance.winner is None:
+        score_board[2] += 1
+    elif game_instance.winner == player1:
+        score_board[0] += 1
+    elif game_instance.winner == player2:
+        score_board[1] += 1
+
+
+def display_stats(player1, player2, score_board):
+    """Display the current game statistics."""
+    print(
+        f"{player1.name}: {score_board[0]} wins, {player2.name}: {score_board[1]} wins, {score_board[2]} ties"
+    )
+
+
+def ask_for_rematch(game_instance):
+    """Ask the players if they want to play again and handle their response."""
+    while True:
+        player_response = input("Would you like to play again? (yes/no): ").lower()
+        if player_response in ["y", "yes"]:
+            game_instance.new_game()
+            game_instance.print_state()
+            return True
+        elif player_response in ["n", "no"]:
+            print("Thanks for playing!")
+            return False
+        else:
+            print("Please enter 'yes' or 'no'. ")
+
+
+if __name__ == "__main__":
+    play_game()
